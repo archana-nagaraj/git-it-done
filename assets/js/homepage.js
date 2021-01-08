bc3f16266586dcccd82646cb448cfbb5297f489c
Archana Nagaraja <johndoe@example.com>
2021-01-08 07:39:46 -0800

    structured the getUserRepo function to acccept user parameter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,27 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos");
-}
-getUserRepos();
+// var getUserRepos = function(){
+//     // describing asynchornous behaviour : fetch the response first and then console log the response. 
+//     fetch("https://api.github.com/users/octocat/repos").then(function(response){
+//         response.json().then(function(data){   // json method formats the response as JSON
+//             console.log(data);
+//         });
+//     });
+//     console.log("outside"); //this sentence is printed first before line 4. It does not wait for thr response to be ready to consolelog thus working asynchronously ( AJAX - Asynchronous JavaScript and XML)
+// }
+
+
+
+var getUserRepos = function(user){
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+};
+
+getUserRepos("microsoft");
+getUserRepos("octocat");
+getUserRepos("gmail");
+getUserRepos("archana-nagaraj");
